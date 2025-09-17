92ea384f30cd198c0e11866ba959692276da2e06
Ayush Sharma <ayushsharma2509@gmail.com>
2025-09-18 03:59:10 +0530

    Initial project setup

---
+++ src/test/java/com/example/SpringbootDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
